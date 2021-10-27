0d6d96e1947ade4f515232677800e1150e1cbe6b
U-DESKTOP-QOHR6QD\Antoine <antoine.cornillot@student-cs.fr>
2021-10-27 15:15:35 +0200

    Add atomic units

--- constants.py
+++ constants.py
@@ -1,6 +1,13 @@
-from scipy.constants import Boltzmann, hbar, m_p
+#Values in SI units
+me_SI = 9.1093837015* 1e-31  # electron mass
+a0_SI = 5.29177210903* 1e-11  # Bohr radius
+hbar_SI = 1.054571817 * 1e-34  # J.s
 
-a = 0.04
-C = 0.3
-V0 = hbar ** 2 / (2 * m_p * a ** 2 * C)
-T = 0.4 * V0 / Boltzmann
+def angstrom2bohr(x):
+    return x / a0_SI * 1e-10
+
+def bohr2angstrom(x):
+    return x * a0_SI / 1e-10
+
+kb = 3.166689e-6  # Ha/K
+m_p = 1836.15267343  # me
